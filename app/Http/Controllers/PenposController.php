2f256978395ab9d3cf7034f13e05b947b59fb3f6
notkillua07 <112600181+notkillua07@users.noreply.github.com>
2023-07-26 01:49:17 +0700

    tidur bro

--- app/Http/Controllers/PenposController.php
+++ app/Http/Controllers/PenposController.php
@@ -9,39 +9,52 @@ use App\Models\Map;
 use App\Models\Result;
 use App\Models\Group;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class PenposController extends Controller
 {
-    public function index(){
-      $user = User::find(Auth::user()->id);
-      $map = DB::table("maps")->select()->where("id", "=", $user->map_id)->first();
-      $result = DB::table("results")->select()->where("post_id", "=", $user->id)->get();
-      $done = array();
-      foreach($result as $temp){
-        array_push($done, $temp->group_id);
-      }
-      $groups = Group::all();
-      $belumMain = array();
-
-      foreach($groups as $temp){
-        if(!in_array($temp->id, $done)){
-          array_push($belumMain, $temp);
+    public function index()
+    {
+        $user = User::find(Auth::user()->id);
+        $map = DB::table("maps")->select()->where("id", "=", $user->map_id)->first();
+        $result = DB::table("results")->select()->where("post_id", "=", $user->id)->get();
+        $done = array();
+        foreach ($result as $temp) {
+            array_push($done, $temp->group_id);
         }
-      }
-      return view("penpos", ["user"=>$user, "groups"=>$groups, "map"=>$map, "result"=>$result, "listGroup"=>$belumMain]);
+        $groups = Group::all();
+        $belumMain = array();
+
+        foreach ($groups as $temp) {
+            if (!in_array($temp->id, $done)) {
+                array_push($belumMain, $temp);
+            }
+        }
+        return view("penpos", ["user" => $user, "groups" => $groups, "map" => $map, "results" => $result, "listGroup" => $belumMain]);
     }
 
-    public function submit(Request $request){
-      $group_id = $request->get("team");
-      $status = $request->get("status");
-      $post_id = Auth::user()->id;
+    public function submit(Request $request)
+    {
 
-      $result = new Result();
-      $result->group_id = $group_id;
-      $result->result = $status;
-      $result->post_id = $post_id;
-      $result->save();
+        $validator = Validator::make($request->all(), [
+            'team' => 'required',
+            'status' => 'required'
+        ]);
+        if ($validator->fails()) {
+            // $request->session()->flash('valid', 'false');
+            return redirect()->route('penpos')->with("status", "Data yang diisikan belum lengkap");
+        } else {
+            $group_id = $request->get("team");
+            $status = $request->get("status");
+            $post_id = Auth::user()->id;
 
-      return redirect()->route('penpos')->with("status", "Input data berhasil");
+            $result = new Result();
+            $result->group_id = $group_id;
+            $result->result = $status;
+            $result->post_id = $post_id;
+            $result->save();
+
+            return redirect()->route('penpos')->with("status", "Input data berhasil");
+        }
     }
 }
